1e0673dafa27e3f9df4e3e75be68ed641843b5d8
vgnshiyer <vgnshiyer@gmail.com>
2023-06-09 22:10:22 -0700

    add: injecting collections (springcore)

---
+++ springcore/src/main/java/com/springcore/collections/Test.java
@@ -0,0 +1,12 @@
+package com.springcore.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/collections/collectionconfig.xml");
+        Emp emp = (Emp) context.getBean("emp");
+        System.out.println(emp);
+    }
+}
