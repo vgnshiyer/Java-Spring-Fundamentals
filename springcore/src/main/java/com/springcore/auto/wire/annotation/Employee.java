17ab68167ebf5c6d36b263ba3cd3748a96fb0b66
vgnshiyer <vgnshiyer@asu.edu>
2023-06-24 12:58:59 -0700

    add: autowired annotation with property (springcore)

---
+++ springcore/src/main/java/com/springcore/auto/wire/annotation/Employee.java
@@ -0,0 +1,27 @@
+package com.springcore.auto.wire;
+
+public class Employee {
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Employee() {
+        super();
+    }
+
+    public Employee(Address address) {
+        this.address = address;
+        System.out.println("Inside constructor");
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [address=" + address + "]";
+    }
+}
